ca16ca53d7819579eab08eb4101bb7926b455038
Rap0d <40348849+Rap0d@users.noreply.github.com>
2020-09-02 16:12:15 +0900

    파일 다운로드 구현

--- src/components/User/Login.vue
+++ src/components/User/Login.vue
@@ -1,20 +1,29 @@
 <template>
   <b-container>
     <h2>Log In</h2>
-    <b-form @submit="onSubmit">
-      <div class="pb-1">
-        <b-input placeholder="Email" v-model="email"></b-input>
+    <div class="row">
+      <div class="col-3">
       </div>
-      <div class="pb-1">
-        <b-input placeholder="Password" v-model="userPw"></b-input>
+      <div class="col-6">
+        <b-form>
+          <div class="pb-1">
+            <b-input placeholder="Email" v-model="email"></b-input>
+          </div>
+          <div class="pb-1">
+            <b-input placeholder="Password" v-model="userPw"></b-input>
+          </div>
+          <div class="pb-1">
+            <b-button class="btn btn-dark" v-on:click="goLogin">Login</b-button>
+          </div>
+          <div class="pb-1 pt-3">
+            <a class="pr-5" v-on:click="goFind">Forgot Password</a>
+            <a v-on:click="goJoin">Join</a>
+          </div>
+        </b-form>
       </div>
-      <div class="pb-1">
-        <b-button type="submit" class="btn-dark">Login</b-button>
+      <div class="col-3">
       </div>
-      <div class="pb-1">
-        <b-button type="join" class="btn-outline-dark">Join</b-button>
-      </div>
-    </b-form>
+    </div>
   </b-container>
 </template>
 
@@ -26,9 +35,26 @@
       userPw: ''
     }),
     methods: {
-      onSubmit() {
+      goLogin: function () {
         console.log(this.email)
         console.log(this.userPw)
+
+        let vm = this
+        let param = {
+          "email": vm.email,
+          "userPw": vm.userPw
+        }
+        vm.$http.post('/api/goLogin/', param).then(function () {
+          console.log('Login Success')
+        }).catch(function (e) {
+          console.log('Fail : ' + e)
+        })
+      },
+      goJoin() {
+        console.log("Join")
+      },
+      goFind() {
+        console.log("Find")
       }
     }
   }
